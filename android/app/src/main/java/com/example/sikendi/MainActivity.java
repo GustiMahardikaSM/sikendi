0e14845ea386b3d34a2a0605c5c127fcd29bb0a1
GustiMahardikaSM <gustimahardikasm@gmail.com>
2026-03-18 11:34:01 +0700

    minor java error

--- android/app/src/main/java/com/example/sikendi/MainActivity.java
+++ android/app/src/main/java/com/example/sikendi/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.sikendi;
 
-import io.flutter.embedding.android.Flut
-ctivity;
+import io.flutter.embedding.android.FlutterActivity;
 
 public class MainActivity extends FlutterActivity {
 }
